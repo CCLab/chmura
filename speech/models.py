2de154d1a54d9c4853f25a9e6d131e64be93b8ba
Janusz A. Urbanowicz <aurbanowicz@centrumcyfrowe.pl>
2012-12-12 16:00:22 +0100

    models fixed

--- speech/models.py
+++ speech/models.py
@@ -5,16 +5,15 @@ from django.db import models
 class Speaker (models.Model):
   name = models.TextField()
   function = models.TextField()
-  head = models.ImageField()
+  head = models.ImageField(upload_to='uploads')
   
 class Context(models.Model):
-  date = models.DateField (auto_add_now=False)
   cabinet = models.TextField()
-  background = models.textField()
   
 class Speech(models.Model):
-  order = models.IntegerField()  
+  date = models.DateField ()  
+  background = models.TextField ()
   
 class Source (models.Model):
-  speech = models.ForeignKey(Speech)
-  url = models.UrlField()
+  speech = models.ForeignKey (Speech)
+  url = models.URLField (max_length=1024, )
--- word/models.py
+++ word/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Ignore (models.Model):
+  word = models.TextField (null=False)
+ 
+class Word (models.Model):
+  word = models.TextField (null=False)
+
+class Synonym (models.Model):
+  origin = models.ForeignKey(Word)
+  word = models.TextField (null=False)  
