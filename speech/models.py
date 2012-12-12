35862944d99795416015a1e16dc5be3c407f7310
Janusz A. Urbanowicz <aurbanowicz@centrumcyfrowe.pl>
2012-12-12 14:15:25 +0100

    model defined

--- speech/models.py
+++ speech/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Speaker (models.Model):
+  name = models.TextField()
+  function = models.TextField()
+  head = models.ImageField()
+  
+class Context(models.Model):
+  date = models.DateField (auto_add_now=False)
+  cabinet = models.TextField()
+  background = models.textField()
+  
+class Speech(models.Model):
+  order = models.IntegerField()  
+  
+class Source (models.Model):
+  speech = models.ForeignKey(Speech)
+  url = models.UrlField()
